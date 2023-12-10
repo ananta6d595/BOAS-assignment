03df2a002cdaa112976259a6aa68ea8112ad1269
Ananta Roy <ananta6d595@gmail.com>
2023-12-10 21:44:00 +0600

    fix: refactor directories os homepage section in HomePage component

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import Hero from "../Components/Hero";
-import FeaturedProduct from "../Components/FeaturedProduct";
-import LevisSection from "../Components/LevisSection";
-import ShopByFitProducts from "../Components/ShopByFitProducts";
-import DownloadAppSection from "../Components/DownloadAppSection";
-import ShopByBrand from "../Components/ShopByBrand";
+import Hero from "../Components/HomePageSections/Hero";
+import FeaturedProduct from "../Components/HomePageSections/FeaturedProduct";
+import LevisSection from "../Components/HomePageSections/LevisSection";
+import ShopByFitProducts from "../Components/HomePageSections/ShopByFitProducts";
+import DownloadAppSection from "../Components/HomePageSections/DownloadAppSection";
+import ShopByBrand from "../Components/HomePageSections/ShopByBrand";
 
 const Homepage = () => {
     // const { isPending, error, data } = useQuery({
